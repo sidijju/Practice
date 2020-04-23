2b8abb96b80056c66c3bca0b43c32b39a55838a0
Siddarth Ijju <dulluijju@gmail.com>
2020-04-22 18:04:11 -0600

    April 22 commit 1

--- 0455a.cpp
+++ 0455a.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-  
+  int n; cin >> n;
+  int arr[n]; vector<int> cnt(100001); vector<int> dp(100001);
+  for(int i = 0; i < n; i ++){
+    cin >> arr[i];
+    cnt[arr[i]]++;
+  }
+  dp[0] = 0;
+  dp[1] = cnt[1];
+  for(int i = 2; i <= n; i ++){
+    dp[i] = max(dp[i-1], dp[i-2]+cnt[i]*i);
+  }
+  cout << dp[n]<< endl;
+  return 0;
 }
